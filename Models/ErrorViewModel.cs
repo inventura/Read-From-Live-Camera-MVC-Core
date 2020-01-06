e829c46636751fe3ec0cde416d3f4e7cdc5b057c
Michal Svoboda <michal.svoboda@kistler.com>
2020-01-06 17:36:56 +0100

    initial commit of the empty ASP.NET Core MVC app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Read_From_Live_Camera_MVC_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
